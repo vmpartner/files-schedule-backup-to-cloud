8d2c723fabcefe4cb1fc6e98816f457bcd821c69
vitams <vmpartner@gmail.com>
2023-01-19 00:50:05 +0300

    Up

--- run.sh
+++ run.sh
@@ -3,7 +3,14 @@
 set -e
 
 if [ "$SCHEDULE" = "**None**" ]; then
-  sh /upload.sh && sleep 4h
+  while true
+  do
+      sh /upload.sh
+      echo ""
+      echo "Sleep 48h..."
+      echo ""
+      sleep 48h
+  done
 else
   echo "$SCHEDULE /upload.sh" > /crontab.txt && \
   /usr/bin/crontab /crontab.txt && \
--- upload.sh
+++ upload.sh
@@ -5,7 +5,14 @@ set -e
 if [ ! -d /source ]; then
   return
 else
-  rclone copy /source "$RCLONE_DEST"
+
+  dt=""
+  if [ ! "$FOLDER_DATE" = "**None**" ]; then
+    dt=`date +$FOLDER_DATE`
+  fi
+
+  echo "Start copy to $RCLONE_DEST/$dt"
+  rclone sync --progress /source "$RCLONE_DEST/$dt"
   if [ "$CHECK_URL" = "**None**" ]; then
     echo "INFO: Define CHECK_URL with https://healthchecks.io to monitor sync job"
   else
